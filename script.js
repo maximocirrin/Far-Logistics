006feb337ecb6f28b88fef1b94e0ea907367a455
maximocirrin <maximocirrin@gmail.com>
2025-02-14 20:31:59 -0300

    desarrollamos sobre nosotros y agregamos glassmorphism a la nav

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target); // Si la animación es de una sola vez
+        }
+      });
+    }, { threshold: 0.1 });
+
+    document.querySelectorAll('.animate-on-scroll').forEach(el => {
+      observer.observe(el);
+    });
+  });
+
