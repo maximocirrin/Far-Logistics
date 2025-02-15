6f4b69ae14405409499f830530febdf5e78b9919
maximocirrin <maximocirrin@gmail.com>
2025-02-15 18:57:51 -0300

    cambios a off canva y agregammos servicios

--- script.js
+++ script.js
@@ -14,3 +14,40 @@
     });
   });
 
+
+
+
+
+
+  // Código nuevo en script.js para manejar múltiples pop-ups
+document.querySelectorAll('.card-button').forEach(button => {
+  button.addEventListener('click', function() {
+    const targetPopup = this.getAttribute('data-popup-target');
+    const popup = document.querySelector(targetPopup);
+    if (popup) {
+      // Reinicia la animación del contenido del pop-up
+      const popupContent = popup.querySelector('.popup-content');
+      popupContent.style.animation = 'none';
+      popupContent.offsetHeight; // Forzar reflow
+      popupContent.style.animation = '';
+      popup.style.display = 'flex';
+    }
+  });
+});
+
+document.querySelectorAll('.popup .close').forEach(closeBtn => {
+  closeBtn.addEventListener('click', function() {
+    const popup = this.closest('.popup');
+    popup.style.display = 'none';
+  });
+});
+
+
+// Nuevo código en script.js para cerrar el pop-up al hacer click afuera del contenido
+document.querySelectorAll('.popup').forEach(popup => {
+  popup.addEventListener('click', function(event) {
+    if (event.target === this) {
+      this.style.display = 'none';
+    }
+  });
+});
